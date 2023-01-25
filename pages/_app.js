acee7e780869cac37647965eb57836235f7ac6ce
alech <hamiltonalec@gmail.com>
2023-01-25 14:29:29 +0000

    custom usernames working

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import Navbar from "@/components/Navbar";
-import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
 import { UserContext } from "@/lib/context";
+import { useUserData } from "@/lib/hooks";
 
 export default function App({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: "alec" }}>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
